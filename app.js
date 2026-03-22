51ede34e5f1c851f33bc69bd689a2d210abfe98e
Vishnu Maddheshiya <vishnumaddheshiya@Vishnus-MacBook-Air.local>
2026-03-22 08:41:41 +0530

    Fix route and update

--- app.js
+++ app.js
@@ -90,6 +90,10 @@ app.use((req,res,next) => {
     next();
 });
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 // app.get("/demouser", async (req,res) => {
 //     let fakeUser = new User({
 //         email: "student@gmail.com",
